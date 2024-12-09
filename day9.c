69d66c09460a8bdbbc93e94ccb59cb3e6d054b19
Drew Guettler <69404804+dbguettler@users.noreply.github.com>
2024-12-09 16:59:51 -0500

    Optimize day 9 by using a doubly linked list

--- day9.c
+++ day9.c
@@ -5,6 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct node_struct {
+  struct node_struct *prev;
+  int id;
+  int file_size;
+  int empty_space_trailing;
+  struct node_struct *next;
+};
+
+typedef struct node_struct Node;
+
 void part1() {
   List *file_lines = get_lines("inputs/day9.txt");
 
@@ -100,104 +110,100 @@ void part2() {
     exit(EXIT_FAILURE);
   }
 
-  int total_blocks = 0;
+  // Copy file IDs into disk
   char *line = file_lines->data[0];
   char num_str[2] = {'\0', '\0'};
-  for (int i = 0; i < strlen(line); i++) {
+  Node *first;
+  Node *last;
+  first = (Node *)malloc(sizeof(Node));
+  first->id = 0;
+  first->prev = NULL;
+  first->next = NULL;
+  num_str[0] = line[0];
+  first->file_size = atoi(num_str);
+  num_str[0] = line[1];
+  first->empty_space_trailing = atoi(num_str);
+  last = first;
+
+  // Create linked list of nodes
+  for (int i = 2; i < strlen(line); i += 2) {
+    Node *current = (Node *)malloc(sizeof(Node));
+    last->next = current;
+    current->id = i / 2;
     num_str[0] = line[i];
-    total_blocks += atoi(num_str);
+    current->file_size = atoi(num_str);
+    num_str[0] = line[i + 1];
+    current->empty_space_trailing = atoi(num_str);
+    current->prev = last;
+    last = current;
   }
 
-  // Copy file IDs into disk
-  int disk[total_blocks];
-  int last_file_end_idx = 0;
-  int current_block = 0;
-  int first_empty = 0;
-  for (int i = 0; i < strlen(line); i++) {
-    int file_id = i / 2;
-    num_str[0] = line[i];
-    int space = atoi(num_str);
-    if (i == 1) {
-      first_empty = current_block;
-    }
-    for (int j = 0; j < space; j++) {
-      if (i % 2 == 0) {
-        // file space
-        disk[current_block] = file_id;
-        last_file_end_idx = current_block;
+  last->next = NULL;
 
-      } else {
-        // free space
-        if (j == 0) {
-          disk[current_block] = -1 * space;
-        } else {
-          disk[current_block] = -1;
-        }
-      }
-      current_block++;
-    }
+  // Find the earliest section of usable space
+  Node *has_space = first;
+  while (has_space->empty_space_trailing <= 0) {
+    has_space = has_space->next;
   }
 
   // Copy files from end to beginning
-  for (int i = disk[last_file_end_idx]; i > 0; i--) {
-    // Get the end position of the file with id i
-    int last_file_idx = last_file_end_idx;
-    while (last_file_idx >= 0 && disk[last_file_idx] == i) {
-      last_file_idx--;
-    }
-    last_file_idx++;
+  Node *current = last;
+  int current_id = current->id;
 
-    int file_size = (last_file_end_idx - last_file_idx + 1);
-
-    // Find the first contiguous space that can accomodate the file located
-    // before the file
-    int empty_idx = first_empty;
-    while (empty_idx < last_file_idx) {
-      if (disk[empty_idx] * -1 >= file_size) {
-        // Space is big enough
-        break;
-      }
-
-      // Space is not big enough. Find the next empty block
-      do {
-        empty_idx++;
-      } while (disk[empty_idx] >= 0);
+  while (current->id != 0) {
+    Node *sufficient_space = has_space;
+    while (sufficient_space != NULL && sufficient_space != current &&
+           sufficient_space->empty_space_trailing < current->file_size) {
+      sufficient_space = sufficient_space->next;
     }
 
-    // empty_idx is either the index of a block that can accomodate the file, or
-    // is equal to the starting index of the file.
-    if (empty_idx < last_file_idx) {
-      int empty_size = disk[empty_idx] * -1;
-      // Copy file.
-      for (int j = 0; j < file_size; j++) {
-        disk[empty_idx + j] = disk[last_file_end_idx];
-        disk[last_file_end_idx] = -1;
-        last_file_end_idx--;
+    if (sufficient_space != NULL && sufficient_space != current) {
+      // We can move this file earlier. Do the following:
+      // Save a pointer to the current file's predecessor.
+      Node *old_predecessor = current->prev;
+      // Add the current file's size and its trailing space to its predecessor.
+      current->prev->empty_space_trailing +=
+          current->file_size + current->empty_space_trailing;
+      // Unlink the current file and link its predecessor and successor.
+      current->prev->next = current->next;
+      if (current->next != NULL) {
+        current->next->prev = current->prev;
       }
-
-      // Update first_empty if empty_idx == first_empty
-      if (first_empty == empty_idx) {
-        first_empty += file_size;
-      }
-
-      // If the next space after the copied location is empty, update the empty
-      // space block size
-      if (disk[empty_idx + file_size] < 0) {
-        disk[empty_idx + file_size] = (empty_size - file_size) * -1;
-      }
+      // Link the current file into the earlier position.
+      current->prev = sufficient_space;
+      current->next = sufficient_space->next;
+      sufficient_space->next = current;
+      current->next->prev = current;
+      // Adjust the trailing space of the current file.
+      current->empty_space_trailing =
+          current->prev->empty_space_trailing - current->file_size;
+      // Reduce the trailing space of the current file's new predecessor to 0.
+      current->prev->empty_space_trailing = 0;
+      // Set the current pointer to the old predecessor to enable finding the
+      // next file to move.
+      current = old_predecessor;
     }
 
-    // Adjust the end index if necessary
-    while (disk[last_file_end_idx] != i - 1) {
-      last_file_end_idx--;
+    // Now, find the next file to move back. It should have an id that is one
+    // less than current_id.
+    while (current->id != current_id - 1) {
+      current = current->prev;
     }
+    current_id--;
   }
 
   long checksum = 0;
-  for (int i = 0; i < total_blocks; i++) {
-    if (disk[i] >= 0) {
-      checksum += i * disk[i];
+  int position = 0;
+  current = first;
+  while (current != NULL) {
+    for (int i = 0; i < current->file_size; i++) {
+      checksum += current->id * (position + i);
     }
+
+    position += current->file_size + current->empty_space_trailing;
+    Node* to_free = current;
+    current = current->next;
+    free(to_free);
   }
 
   printf("Part 2 Checksum: %ld\n", checksum);
